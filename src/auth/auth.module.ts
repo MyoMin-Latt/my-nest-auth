2511c99469018df4d4d551493e2bb1983b060da0
Myo Min Latt <myominlatt330@gmail.com>
2025-03-08 09:00:40 +0630

    Generate Token > auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_SECRET } from 'src/configs/jwt-secret';
 
 @Module({
   providers: [AuthService, UsersService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
 })
 export class AuthModule {}
---
+++ src/configs/jwt-secret.ts
@@ -0,0 +1,5 @@
+// FIXME: This file is for development purposes only. Do not use this in production.
+// FIXME: Replace this with a proper configuration management system.
+
+export const JWT_SECRET =
+    "if-at-first-you-don't-succeed-dust-yourself-off-and-try-again";
